f11825e364bd6e595a25b4fa31c05a186822c962
ncnhan <ncn@LAPTOP-2AO5V101.localdomain>
2025-10-03 14:25:31 +0700

    first commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *pgxpool.Pool
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	defer testDB.Close() // đảm bảo đóng pool sau khi chạy test
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
