c3634e4de882b16ca25646a7dae0aa7a0c5284b7
ncnhan <ncn@LAPTOP-2AO5V101.localdomain>
2025-10-25 00:45:16 +0700

    ci deploy

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 set -e
 
 echo "Running migrations..."
-source /app/app.env
+# source /app/app.env dùng cái này để load cấu hình trong env cx dc
+# hoặc docker run -p 8080:8080 -e DB_SOURCE=... để truyền biến môi trường 
+# sau khi pull image về
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "Starting app..."
